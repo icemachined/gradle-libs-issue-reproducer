808ea0ce505ddb56c83d56baef8208c3548016df
Dmitry Morozovsky <27895587+icemachined@users.noreply.github.com>
2022-07-05 16:29:02 +0300

    reproducer with libs cyclic rebendency gradle build crash

---
+++ src/main/kotlin/com/example/springgradlebug/SpringgradlebugApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springgradlebug
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringgradlebugApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringgradlebugApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springgradlebug/SpringgradlebugApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springgradlebug
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringgradlebugApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
